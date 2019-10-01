6a887a59219df243b3b7bf7354304943a2648e06
Charles Hessel <hesselcharles@gmail.com>
2019-10-01 22:07:00 +0200

    remove copyright

--- gf_multichannel.m
+++ gf_multichannel.m
@@ -7,20 +7,7 @@ function v = gf_multichannel(u, epsilon, radius)
 % This function uses "conv2" rather that "imboxfilt" for the box filter, so that
 % it can be executed with Octave (imboxfilt is only in Matlab).
 %
-% Copyright (C) 2019, Charles Hessel <charles.hessel@cmla.ens-cachan.fr>
-%
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU Affero General Public License as published
-% by the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-%
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU Affero General Public License for more details.
-%
-% You should have received a copy of the GNU Affero General Public License
-% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% 2019, Charles Hessel, CMLA, ENS Paris-Saclay
 
 if ~exist('epsilon', 'var') || isempty(epsilon), epsilon = 0.004^2; end
 if ~exist('radius', 'var')  || isempty(radius),  radius = 1;        end
